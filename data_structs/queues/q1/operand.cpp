eb830c9992e09def15424fdc95d6fb96f0fbfb4d
Raiyan Sayeed <raiyan.business@gmail.com>
2019-10-29 23:03:43 +0000

    ahhh

--- data_structs/queues/q1/operand.cpp
+++ data_structs/queues/q1/operand.cpp
@@ -4,24 +4,149 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <math.h>
 
 using namespace std;
 
+void showstack(stack <int> s)
+{
+    while (!s.empty())
+    {
+        cout << '\t' << s.top();
+        s.pop();
+    }
+    cout << '\n';
+}
+
+void showstackChar(stack <char> s)
+{
+    while (!s.empty())
+    {
+        cout << '\t' << s.top();
+        s.pop();
+    }
+    cout << '\n';
+}
+
 int main() {
     stack <int> nums;
-    stack <string> ops;
+    stack <char> ops;
     stringstream ss;
     string exp;
+    string final;
 
     cout << "Input an expression to evaluate" << endl;
-    getline(cin, exp);
+    //getline(cin, exp);
+    exp = "3+5/3+2^3";
 
     exp.erase(remove_if(exp.begin(), exp.end(), ::isspace), exp.end());
 
-    cout << exp;
 
-    for (int i = 0; i < exp.size(); i++) {
+    cout << exp << endl;
 
+    int x = 0;
+
+    for (int x = 0; x < exp.size(); x++) {
+        if (exp[x] != '+' || exp[x] != '-' || exp[x] != '/' || exp[x] != '*' || exp[x] != '^') {
+            nums.push(exp[x] - '0');
+        } else {
+            ops.push(exp[x]);
+            if (ops.top() == '/') {
+                int second = nums.top();
+                nums.pop();
+                int first = nums.top();
+                cout << first << " / " << second << endl;
+                nums.push(first / second);
+            }
+            else if (ops.top() == '*') {
+                int second = nums.top();
+                nums.pop();
+                int first = nums.top();
+                cout << first << " * " << second << endl;
+                nums.push(first * second);
+            }
+            else if (ops.top() == '^') {
+                int second = nums.top();
+                nums.pop();
+                int first = nums.top();
+                cout << first << " ^ " << second << endl;
+                nums.push(pow(first, second));
+            }
+        }
     }
+
+    showstack(nums);
+    showstackChar(ops);
+
+
+
+
+    // for (int i = 1; i < exp.size(); i++) {
+    //     switch (exp[i]) {
+    //         case '*': {
+    //             ops.push('*');
+    //             break;
+    //         }
+    //         case '/': {
+    //             ops.push('/');
+    //             break;
+    //         }
+    //         case '^': {
+    //             ops.push('^');
+    //             break;
+    //         }
+    //         case '+': {
+    //             ops.push('+');
+    //             break;
+    //         }
+    //         case '-': {
+    //             ops.push('-');
+    //             break;
+    //         }
+
+    //         default : {
+    //             nums.push(exp[i]);
+    //             for (int j = 0; j < ops.size(); j++) {
+    //             switch (ops) {
+    //                 case '*': {
+    //                     int x = nums.top();
+    //                     nums.pop();
+    //                     int y = nums.top();
+    //                     nums.push(y * x);
+    //                     break;
+    //                 }
+    //                 case '/': {
+    //                     int x = nums.top();
+    //                     nums.pop();
+    //                     int y = nums.top();
+    //                     nums.push((y / x));
+    //                     break;
+    //                 }
+    //                 case '^': {
+    //                     int x = nums.top();
+    //                     nums.pop();
+    //                     int y = nums.top();
+    //                     nums.push(pow(y, x));
+    //                     break;
+    //                 }
+    //                 case '+': {
+    //                     int x = nums.top();
+    //                     nums.pop();
+    //                     int y = nums.top();
+    //                     nums.push(y + x);
+    //                     break;
+    //                 }
+    //                 case '-': {
+    //                     int x = nums.top();
+    //                     nums.pop();
+    //                     int y = nums.top();
+    //                     nums.push(y - x);
+    //                     break;
+    //                 }
+    //             }
+    //             }
+    //         }
+    //     }
+    // }
 
 }
