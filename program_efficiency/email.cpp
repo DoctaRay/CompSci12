c19a94399cf63ecbd33a5a65ba76eaf15839571b
Raiyan Sayeed <raiyan.business@gmail.com>
2019-11-13 20:07:59 +0000

    efficiency done

--- program_efficiency/email.cpp
+++ program_efficiency/email.cpp
@@ -1,10 +1,13 @@
-// package email;
+/*
+- Implement quick sort
+- Implement binary search
+- 
+*/
 
-// import java.io.*;
-// import java.util.*;
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bits/stdc++.h> 
 #include <algorithm>
 #include <unistd.h>
 #include <time.h>
@@ -12,79 +15,115 @@
 
 using namespace std;
 
-//refactor
-void ReplaceAll(std::string& str, const std::string& from, const std::string& to) {
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-    }
-}
-
-// public class Email {
 int main(int argc, const char** argv) {
     clock_t start = clock();
-
-//     public static void main(String[] args) throws FileNotFoundException {
-//       Scanner input = new Scanner(new File("data11.txt"));  //data.txt  for test data
     ifstream myFile;
     myFile.open("data11.txt");
 
-//       ArrayList<String> unique = new ArrayList<String>();
-    vector<string> unique;
+
+    unordered_set<string> unique;
     string email;
-    string first;
-    string second;
+    string first = "";
+    string second ="";
     int plusLoc;
-//        while(input.hasNext()){
+    bool isInside = false;
+    int count = 0;
+
     while(!myFile.eof()){
         //usleep(3000000);
 
-//        int num = input.nextInt();
+
         int num;
         myFile >> num;
-        //cout << num << endl;
+        //printf("%d\n", num);
 
-
-//        for(int c=0;c<num;c++){
         for (int c = 0; c < num; c++) {
-//            String email = input.next().toLowerCase();           //case insensitive
+            //printf("*********\n");
+
                 myFile >> email;
-                transform(email.begin(), email.end(), email.begin(), [](unsigned char c){ return std::tolower(c); });
-//            String first=email.substring(0,email.indexOf("@"));  //email before the @ symbol
-                first = email.substr(0, email.find("@"));
-//            first=first.replaceAll("\\.", "");                   //dots before @ ignored
-                ReplaceAll(first, ".", "");
-                //cout << first << endl;
-//            String second = email.substring(email.indexOf("@")); //email after the @ symbol
-                second = email.substr(email.find("@"));
-                // cout << second << endl;
-                // cout << first + second << endl;
 
-
-//            int plusLoc=first.indexOf("+");                      //+ before @ location in string
-                plusLoc = first.find("+");
-//            if(plusLoc>=0)first=first.substring(0,plusLoc);     //+ and string after ignored
-                if(plusLoc>= 0) first = first.substr(0, plusLoc);
-//            if(!unique.contains(first+second))unique.add(first+second);  //track unique emails in an array
-                if(find(unique.begin(), unique.end(), first+second) == unique.end()) {
-                    //cout << "works!" << endl;
-                    unique.push_back(first+second);
+                for (int i = 0; i < email.length(); i++) {
+                    email[i] = tolower(email[i]);
                 }
+                //printf("lowercase email %s\n", email.c_str());
+
+
+                for (int i = 0; i < email.length(); i++) {
+                    if (email[i] == '@') {
+                        //first.push_back(email[i]);
+                        break;
+                    }
+                    first.push_back(email[i]);
+                }
+                //printf("pre email %s\n", first.c_str());
+
+                for (int i = first.length() + 1; i < email.length(); i++) {
+                    second.push_back(email[i]);
+                }
+                //printf("post email %s\n", second.c_str());
+
+                for (int i = 0; i < first.length(); i++) {
+                    if (first[i] == '.') {
+
+                        //printf("yeet %c \n", first[i]);
+                        first.erase(i, 1);
+                        //break;
+                    }
+                }
+                //printf("removing dots %s\n", first.c_str());
+                
+
+                for (int i = 0; i < first.length(); i++) {
+                    if (first[i] == '+') {
+                        plusLoc = i;
+                 //       //printf("found + at %d\n", plusLoc);
+                        break;
+                    }
+                }
+
+                if(plusLoc> 0) {
+                    string temp = "";
+                    //first="";
+                    //first = first.substr(0, plusLoc);
+                    for (int i = 0; i < plusLoc; i++) {
+                        temp.push_back(first[i]);
+                    }
+                 //   //printf("%d, %s\n", plusLoc, temp.c_str());
+                    first = temp;
+                }
+
+                // for (int i = 0; i < unique.size(); i++) {
+                //     if (unique[i].compare(first+second) ==0) {
+                //         isInside = true;
+                //         break;
+                //     }
+                // ////printf("%s, %s\n", unique[i].c_str(), (first+second).c_str());
+                // ////printf(isInside ? "true\n" : "false\n");
+                // }
+
+                if(unique.find(first+second) == unique.end()) {
+                    //printf("final %s \n", (first + second).c_str());
+                    unique.insert(first+second);
+                } else {
+                    //printf("else %s \n", (first + second).c_str());
+                }
+                //printf("joop \n");
 //        }
+            first="";
+            second = "";
+            isInside = false;
+            plusLoc = -1;
+            //printf("%zu\n", unique.size());
         }
-//        System.out.println(unique.size());  //number of unique emails is the amount in the array
-        //cout << unique.size() << endl;
-        //int n = (int)unique.size();
-        printf("%d\n", (int)unique.size());
-        //cout << "--------------------" << endl;
-//        unique.clear();  //clear the array for next data set
+
+        printf("founded %d\n", (int)unique.size());
+        //printf("------------------------------\n");
+
+
         unique.clear();
-//        }
 
-//     }
-// }
-
+        // count++;
+        // if (count == 3) break;
     }
     printf("Time taken: %.2fs\n", (double)(clock() - start)/CLOCKS_PER_SEC);
     return 0;
