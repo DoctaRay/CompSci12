a0b9dfd85ea8e4ae96b8ad9c2b590b4826905e39
Raiyan Sayeed <raiyan.business@gmail.com>
2019-11-16 22:28:44 +0000

    starting node structure

--- graphs/CCC_2019_juniorEF_questions/q5/j5/q5.cpp
+++ graphs/CCC_2019_juniorEF_questions/q5/j5/q5.cpp
@@ -8,18 +8,18 @@
 using namespace std;
 
 struct Replacement {
+    int ruleNum;
     int index;
-    int charNum;
 };
 
 struct Node {
     int prev; //indices of previous coordinate string
-    int next;
+    //int next;
     Replacement how; // how it was done
     string curr;
 };
 
-void findAllOccurances(std::string data, std::string toSearch [2], vector<string> &nodeLevel)
+void findAllOccurances(std::string data, std::string toSearch [2], int ruleNum, int prevIndex, vector<Node> &nodeLevel)
 {
 	// Get the first occurrence
 	size_t pos = data.find(toSearch[0]);
@@ -28,12 +28,17 @@ void findAllOccurances(std::string data, std::string toSearch [2], vector<string
 	// Repeat till end is reached
 	while( pos != std::string::npos)
 	{
+        Node t;
+        t.how.ruleNum = ruleNum;
+        t.how.index = pos;
         string temp = data;
 		// Add position to the vector
 		//vec.push_back(pos);
         temp.replace(pos, toSearch[0].length(), toSearch[1]);
-        nodeLevel.push_back(temp);
         cout << temp << endl;
+        t.curr = temp;
+        t.prev = prevIndex;
+        nodeLevel.push_back(t);
  
 		// Get the next occurrence from the current position
 		pos =data.find(toSearch[0], pos + toSearch[0].size());
@@ -53,17 +58,20 @@ int main(int argc, const char** argv) {
     string one [2], two [2], three[2];
     int steps;
 
-    file >> one[0] >> one[1];
-    file >> two[0] >> two[1];
-    file >> three[0] >> three[1];
+    string rules [3][2];
+
+    file >> rules[0][0] >> rules[0][1];
+    file >> rules[1][0] >> rules[1][1];
+    file >> rules[2][0] >> rules[2][1];
+
 
     file >> steps >> init >> fin;
 
     printf("%s %s \n%s %s \n%s %s \n%d %s %s\n\n", one[0].c_str(), one[1].c_str(), two[0].c_str(), two[1].c_str(), three[0].c_str(), three[1].c_str(), steps, init.c_str(), fin.c_str());
 
-    vector<vector<string> > list;
+    vector<vector<Node> > list;
     for (int i = 0; i < steps + 1; i++) {
-        vector<string> t;
+        vector<Node> t;
         list.push_back(t);
     }
 
@@ -78,20 +86,25 @@ int main(int argc, const char** argv) {
 
     // cout << init << endl;
 
-    list[0].push_back(init);
+    Node t;
+    t.how.index = 0;
+    t.how.ruleNum = 0;
+    t.curr = init;
+    t.prev = 0;
+    list[0].push_back(t);
 
     for (int j = 0; j + 1< list.size(); j++) {
-        vector<string> x = list[j];
+        vector<Node> x = list[j];
         cout << x.size() << endl;
         for (int i = 0; i < x.size(); i++) {
             cout << "iter " << j << " " << i << endl;
-            cout << x[i] << endl;
+            cout << x[i].curr << endl;
             cout << "One" << endl;
-            findAllOccurances(x[i], one, list[j+1]);
+            findAllOccurances(x[i].curr, one, 1, i, list[j+1]);
             cout << "Two" << endl;
-            findAllOccurances(x[i], two, list[j+1]);
+            findAllOccurances(x[i].curr, two, 2, i, list[j+1]);
             cout << "Three" << endl;
-            findAllOccurances(x[i], three, list[j+1]);
+            findAllOccurances(x[i].curr, three, 3, i, list[j+1]);
             cout << "------------------------" << endl;
         }
         cout << list[j+1].size() << endl;
