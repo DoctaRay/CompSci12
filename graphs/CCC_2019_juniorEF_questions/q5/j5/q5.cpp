f8b6a2a1140a48272c7dac2831f372aa14b52f09
Raiyan Sayeed <raiyan.business@gmail.com>
2019-11-16 20:31:33 +0000

    figures out different variations

--- graphs/CCC_2019_juniorEF_questions/q5/j5/q5.cpp
+++ graphs/CCC_2019_juniorEF_questions/q5/j5/q5.cpp
@@ -7,11 +7,38 @@
 
 using namespace std;
 
+struct Replacement {
+    int index;
+    int charNum;
+};
+
 struct Node {
-    string prev;
-    string next;
+    int prev; //indices of previous coordinate string
+    int next;
+    Replacement how; // how it was done
     string curr;
 };
+
+void findAllOccurances(std::string data, std::string toSearch [2], vector<string> &nodeLevel)
+{
+	// Get the first occurrence
+	size_t pos = data.find(toSearch[0]);
+
+ 
+	// Repeat till end is reached
+	while( pos != std::string::npos)
+	{
+        string temp = data;
+		// Add position to the vector
+		//vec.push_back(pos);
+        temp.replace(pos, toSearch[0].length(), toSearch[1]);
+        nodeLevel.push_back(temp);
+        cout << temp << endl;
+ 
+		// Get the next occurrence from the current position
+		pos =data.find(toSearch[0], pos + toSearch[0].size());
+	}
+}
 
 int main(int argc, const char** argv) {
     //int vertices, edges;
@@ -32,19 +59,43 @@ int main(int argc, const char** argv) {
 
     file >> steps >> init >> fin;
 
-    printf("%s %s %s %s %s %s %d %s %s", one[0].c_str(), one[1].c_str(), two[0].c_str(), two[1].c_str(), three[0].c_str(), three[1].c_str(), steps, init.c_str(), fin.c_str());
+    printf("%s %s \n%s %s \n%s %s \n%d %s %s\n\n", one[0].c_str(), one[1].c_str(), two[0].c_str(), two[1].c_str(), three[0].c_str(), three[1].c_str(), steps, init.c_str(), fin.c_str());
 
-    vector<vector<Node> > list;
-    for (int i = 0; i < steps; i++) {
-        vector<Node> t;
+    vector<vector<string> > list;
+    for (int i = 0; i < steps + 1; i++) {
+        vector<string> t;
         list.push_back(t);
     }
 
-    while (true) {
-        index = init.find(one[0]);
-        if (index != string::npos) {
-            init.replace(one[1], index, one[1].length());
+    vector<size_t> vec; 
+    // while (true) {
+    //     index = init.find(one[0]);
+    //     if (index != string::npos) {
+    //         init.replace(index, 1, one[1]);
+    //         break;
+    //     }
+    // }
+
+    // cout << init << endl;
+
+    list[0].push_back(init);
+
+    for (int j = 0; j + 1< list.size(); j++) {
+        vector<string> x = list[j];
+        cout << x.size() << endl;
+        for (int i = 0; i < x.size(); i++) {
+            cout << "iter " << j << " " << i << endl;
+            cout << x[i] << endl;
+            cout << "One" << endl;
+            findAllOccurances(x[i], one, list[j+1]);
+            cout << "Two" << endl;
+            findAllOccurances(x[i], two, list[j+1]);
+            cout << "Three" << endl;
+            findAllOccurances(x[i], three, list[j+1]);
+            cout << "------------------------" << endl;
         }
+        cout << list[j+1].size() << endl;
+        cout << "*******************" << endl;
     }
 
     return 0;
