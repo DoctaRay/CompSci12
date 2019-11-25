8535617efc863c27a98201cd189164be17ff5ef0
Raiyan <raiyan.business@gmail.com>
2019-11-24 22:05:29 -0500

    mrap

--- graphs/CCC_2019_juniorEF_questions/ecoo/ecoo.cpp
+++ graphs/CCC_2019_juniorEF_questions/ecoo/ecoo.cpp
@@ -1,3 +1,42 @@
 #include <vector>
 #include <cstdlib>
+#include <fstream>
+#include <set>
+#include <unordered_map>
 
+using namespace std;
+
+struct Node {
+	Node *motherNode;
+	string name;
+	int numSisters = 0;
+	int numCousins = 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	ifstream file;
+	file.open("ecoo20.txt");
+
+	unordered_map<string, vector<string> > list;
+
+	int nodeNum;
+
+	cin >> nodeNum;
+
+	set<string, vector<string> > list;
+
+	Node nodeArr [nodeNum];
+
+	for (int i = 0; i < ansNum; i++) {
+		Node mother;
+		Node daughter;
+		cin >> mother.name;
+		cin >> daughter.name;
+		daughter.motherNode = &mother;
+		nodeArr[i] = daughter;
+	}
+
+	
+
+}
