42babd579ff98a70c79d5f9bf9b31139990ecb62
Raiyan Sayeed <yoboi7585@gmail.com>
2019-11-29 10:23:40 -0500

    started strategic bombing problem

--- graphs/traversal/CC 01 S3 - Strategic Bombing/bomb.cpp
+++ graphs/traversal/CC 01 S3 - Strategic Bombing/bomb.cpp
@@ -1,9 +1,9 @@
 //Modified version of http://www.geeksforgeeks.org/breadth-first-traversal-for-a-graph/
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <list>
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <list>
 #include <fstream>
 #include <execinfo.h>
 #include <signal.h>
@@ -15,11 +15,11 @@ using namespace std;
 //   void *array[10];
 //   size_t size;
 
-//   // get void*'shahirIndex for all entries on the stack
+//   // get void*'s for all entries on the stack
 //   size = backtrace(array, 10);
 
 //   // print out all the frames to stderr
-//   fprintf(stderr, "Error: signal %dateIndex:\n", sig);
+//   fprintf(stderr, "Error: signal %d:\n", sig);
 //   backtrace_symbols_fd(array, size, STDERR_FILENO);
 //   exit(1);
 // }
@@ -42,22 +42,22 @@ public:
         Edge[v].push_back(w);
     }
 
-    string BFS(int shahirIndex, int dateIndex)
+    string BFS(int s, int d)
     {
         for(auto i : visited) i = false;
         list<int> q;
-        visited[shahirIndex] = true;
-        q.push_back(shahirIndex);
+        visited[s] = true;
+        q.push_back(s);
         while (!q.empty())
         {
-            shahirIndex = q.front();
-            if (shahirIndex == dateIndex) {
+            s = q.front();
+            if (s == d) {
                 return "GO SHAHIR!";
             }
-            //cout<<shahirIndex<<" ";
+            //cout<<s<<" ";
             //cout << "yeet" << endl;
             q.pop_front();
-            for(auto i : Edge[shahirIndex])
+            for(auto i : Edge[s])
             {
                 if(!visited[i])
                 {
@@ -78,35 +78,5 @@ public:
 int main()
 {
     //signal(SIGSEGV, handler);   // install our handler
-    ifstream file;
-    file.open("data.txt");
-    int numNode, numPaths, shahirIndex, dateIndex;
-
-    file >> numNode >> numPaths >> shahirIndex >> dateIndex;
-    //cout << numNode << numPaths << shahirIndex << dateIndex << endl << endl;
-
-    Graph g(numNode);
-    int x, y;
-    for (int i = 1; i <= numPaths; i++) {
-        //cout << x << " " << y << endl;
-        //cout << "not me " << endl;
-        file >> x >> y;
-        g.add_edge(x, y);
-    }
-    //cout << endl;
-
-    cout << g.BFS(shahirIndex, dateIndex) << endl;;
-    //cout << "lil" << endl;
-    // Graph g(6);
-    // g.add_edge(0,1);
-    // g.add_edge(0,2);
-    // g.add_edge(1,2);
-    // g.add_edge(2,0);
-    // g.add_edge(2,3);
-    // g.add_edge(3,3);
-    // int v;
-    // cout<<"Enter vertex from where to perform BFS"<<endl;
-    // cin>>v;
-    // g.BFS(2);
-    // cout<< (char)true;
+    
 }
