87eba5e0ef4b09919a19f53e7f735c7e5cdba1a1
Raiyan <raiyan.business@gmail.com>
2019-12-01 20:24:23 -0500

    getting there

--- graphs/shortest_path/bfs.cpp
+++ graphs/shortest_path/bfs.cpp
@@ -7,19 +7,52 @@ using namespace std;
 
 class Graph 
 {
-	int V;
-	list<int> *adj;
+	vector<vector<int> > Edge;
+	vector<unsigned char> visited;
 
 public: 
-	Graph(int V);
-	void addEdge(int s, int d);
+	Graph(int V)
+	{
+		Edge.resize(V);
+		visited.resize(V);
+	}
+
+	void addEdge(int s, int d)
+	{
+		Edge[s].push_back(d);
+		Edge[d].push_back(s);
+	}
+
+	int bfs(int start, int end) {
+		for (auto i: visited) i = false;
+
+		list<int> q;
+		visited[start] = true;
+		q.push_back(start);
+		int count = 0;
+
+		while (!q.empty()) 
+		{
+			start = q.front();
+			count++;
+			if (start == end) {
+				//cout << "count: " << count << endl;
+				return count;
+			}
+			q.pop_front();
+			for (auto i : Edge[start]) 
+			{
+				if (!visited[i])
+				{
+					visited[i] = true;
+					q.push_back(i);
+				} 
+			}
+		}
+		return -1;
+	}
 
 };
-
-void Graph::addEdge(int s, int d) 
-{
-
-}
 
 int main(int argc, char const *argv[])
 {
@@ -27,18 +60,58 @@ int main(int argc, char const *argv[])
 	file.open("data.txt");
 
 	int numHouses, numRoads, numErrands, houseC;
-	int roads [numRoads][2];
-	int errands [numErrands][2];
 	
 	file >> numHouses >> numRoads >> numErrands >> houseC;
 	
+	int roads [numRoads][2];
+	int errands [numErrands][2];
+
+	Graph g(numRoads);
+	//cout << "numHouses: " << numHouses << endl;
+
+	int x, y;
 	for (int i = 0; i < numRoads; ++i) {
-		file >> roads[i][0] >> roads[i][1];
+		file >> x >> y;
+		//cout << x << " " << y << endl;
+		cout << i << endl;
+		g.addEdge(x,y);
 	}
 
+	cout << "numErrands: " << numErrands << endl;
+	int c, d;
 	for (int i = 0; i < numErrands; ++i) {
-		file >> errands[i][0] >> errands[i][1];
+		//		cout << "yikes" << endl;
+		file >> c >> d;
+		cout << "c: " << c << endl;
+		cout << "d: " << d << endl;
+		int dest1 = g.bfs(houseC, c);
+		//cout << dest1<<endl;
+		int dest2 = g.bfs(houseC, d);
+		//cout << dest2<<endl;
+		int total = dest1 + dest2;
+
+		if (dest1 < 0 || dest2 < 0) {
+			cout << "This is a scam!" << endl;
+		} else {
+			cout << total << endl;
+		}
 	}  
+	//cout << "lulu" << endl;
+
+	// for (int i = 0; i < numRoads; ++i) {
+	// 	cout << i << endl;
+	// }
+
+	// for (int i = 0; i < numErrands; ++i) {
+	// 	int dest1 = g.bfs(houseC, errands[i][0]);
+	// 	int dest2 = g.bfs(houseC, errands[i][1]);
+
+	// 	if (dest1 < 0 || dest2 < 0) {
+	// 		cout << "This is a scam!" << endl;
+	// 	} else {
+	// 		cout << dest1 + dest2 << endl;
+	// 	}
+	// }  
 
 
 	
