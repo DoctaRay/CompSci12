7a36ead8e746a3e9d350fe522909943e71b058a7
Md Raiyan bin Sayeed <mdraiyanbinsayeed@Mds-MacBook-Pro.local>
2020-01-20 12:44:23 -0500

    done shortest path

--- graphs/shortest_path/bfs.cpp
+++ graphs/shortest_path/bfs.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <queue>
 
 using namespace std;
 
-class Graph 
+class Graph
 {
-	vector<vector<int> > Edge;
+	vector<vector<int>> Edge;
 
-public: 
+public:
 	Graph(int V)
 	{
 		Edge.resize(V);
@@ -23,64 +24,53 @@ public:
 	void addEdge(int s, int d)
 	{
 		Edge[s].push_back(d);
-		printf("%d -> %d\n", s, d);
+		Edge[d].push_back(s);
+		//printf("%d -> %d\n", s, d);
 		//Edge[d].push_back(s);
 	}
 
-	int bfs(int start, int end) {
-		vector<unsigned char> visited;
-		visited.resize(Edge.size());
-		cout << "start: " << start << endl;
-		cout << "end: " << end << endl;
-		if (start == end) return 0;
-		for (auto i: visited) i = false;
+	int bfs(int start, int end)
+	{
+		vector<bool> visited(Edge.size(), 0);
 
-		list<int> q;
+		vector<int> distance(Edge.size(), 0);
+
+		queue<int> q;
+		distance[start] = 0;
+
+		q.push(start);
 		visited[start] = true;
-		q.push_back(start);
-		int count = 0;
-		// if (start == end) {
-		// 			//cout << "count: " << count << endl;
-		// 	return count;
-		// }
-
-		while (!q.empty()) 
+		while (!q.empty())
 		{
-			start = q.front();
-			//cout << "start:" << start << endl;
-			count++;
-			q.pop_front();
-			for (auto i : Edge[start]) 
+			int x = q.front();
+			q.pop();
+
+			for (int i = 0; i < Edge[x].size(); i++)
 			{
-				if (!visited[i])
-				{
-					visited[i] = true;
-					cout << "i: " << i << endl;
-					if (i == end) {
-					//cout << "count: " << count << endl;
-						cout << "bloop" << endl;
-						return count;
-					} 
-					q.push_back(i);
-				} 
+				if (visited[Edge[x][i]])
+					continue;
+
+				// update distance for i
+				distance[Edge[x][i]] = distance[x] + 1;
+				q.push(Edge[x][i]);
+				visited[Edge[x][i]] = 1;
 			}
 		}
-		return -1;
+		return distance[end];
 	}
-
 };
 
 int main(int argc, char const *argv[])
 {
 	ifstream file;
-	file.open("data.txt");
+	file.open("data2.txt");
 
 	int numHouses, numRoads, numErrands, houseC;
-	
+
 	file >> numHouses >> numRoads >> numErrands >> houseC;
-	
-	int roads [numRoads][2];
-	int errands [numErrands][2];
+
+	int roads[numRoads][2];
+	int errands[numErrands][2];
 
 	Graph g(numHouses + 1);
 	//cout << "numHouses: " << numHouses << endl;
@@ -88,36 +78,41 @@ int main(int argc, char const *argv[])
 	int x, y;
 
 	g.graphSize();
-	
-	for (int i = 1; i <= numRoads; ++i) {
+
+	for (int i = 1; i <= numRoads; ++i)
+	{
 		file >> x >> y;
 		//cout << x << " " << y << endl;
-		cout << i << endl;
-		g.addEdge(x,y);
+		//cout << i << endl;
+		g.addEdge(x, y);
 	}
 
-	cout << "numErrands: " << numErrands << endl;
+	//cout << "numErrands: " << numErrands << endl;
 	int c, d;
-	for (int i = 0; i < numErrands; ++i) {
+	for (int i = 0; i < numErrands; ++i)
+	{
 		//		cout << "yikes" << endl;
 		file >> c >> d;
-		cout << "c: " << c << endl;
-		cout << "d: " << d << endl;
-		int dest1 = g.bfs(houseC, c);
-		cout << "dest1: " << dest1<<endl;
-		cout << "------------------" << endl;
+		// cout << "c: " << c << endl;
+		// cout << "d: " << d << endl;
+		int dest1 = g.bfs(c, houseC);
+		//  cout << "dest1: " << dest1<<endl;
+		//  cout << "------------------" << endl;
 		int dest2 = g.bfs(houseC, d);
-		cout << "dest2:  " << dest2<<endl;
-		cout << "------------------" << endl;
+		//  cout << "dest2:  " << dest2<<endl;
+		//  cout << "------------------" << endl;
 		int total = dest1 + dest2;
 
-		if (dest1 < 0 || dest2 < 0) {
+		if (dest1 <= 0 || dest2 <= 0)
+		{
 			cout << "This is a scam!" << endl;
-		} else {
+		}
+		else
+		{
 			cout << total << endl;
 		}
-		cout << "****************" << endl;
-	}  
+		//cout << "****************" << endl;
+	}
 	//cout << "lulu" << endl;
 
 	// for (int i = 0; i < numRoads; ++i) {
@@ -133,9 +128,7 @@ int main(int argc, char const *argv[])
 	// 	} else {
 	// 		cout << dest1 + dest2 << endl;
 	// 	}
-	// }  
+	// }
 
-
-	
 	return 0;
 }
